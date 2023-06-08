549d0a3162c75ddaee66a796b6a07cf88c6a33c9
Poluvko-Tatyana <poluvko.tatyana@gmail.com>
2023-06-08 14:24:08 +0300

    1 завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,15 +4,18 @@ stopButton.disabled = true;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
+const timerId = null;
 
 startButton.addEventListener('click', onClickStart)
 function onClickStart(evt){
-   const timerId = setInterval(() => {
+   timerId = setInterval(() => {
       document.body.style.backgroundColor = getRandomHexColor()
     },1000)
     startButton.disabled = true;
     stopButton.disabled = false;
 }
+
+
 stopButton.addEventListener("click", () => {
     clearInterval(timerId);
     startButton.disabled = false;
